8f09e120ac059e552ac18d86bcfb376e3b1ee4d6
lsl <lsl991126@163.com>
2022-03-10 17:00:45 +0800

    添加Button1事件

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.gittest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.gittest.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(activityMainBinding.getRoot());
+        initView();
+    }
+
+    private void initView() {
+        activityMainBinding.btn1.setOnClickListener(view -> Toast.makeText(MainActivity.this, "弹框1", Toast.LENGTH_SHORT).show());
     }
 }
